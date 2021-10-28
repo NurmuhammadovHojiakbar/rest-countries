0ed7ffd45cad2946e79506dada8442875af5cbd7
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-10-28 12:53:11 +0500

    not found image added

--- src/components/CountryCardItem/CountryCardItem.js
+++ src/components/CountryCardItem/CountryCardItem.js
@@ -7,7 +7,7 @@ const CountryCardItem = ({country}) => {
             <Link className="country-card__link" to={country.name}>
                 <img
                     className="country-card__flag"
-                    src={country.flag}
+                    src={country.flag? country.flag : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"}
                     alt={country.name}
                     width="265"
                     height="160"
